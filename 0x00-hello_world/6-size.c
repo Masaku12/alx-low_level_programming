dac61e3c6f057179890ad78ffff759bee902b62f
Ambrose Matata Sammy <ambrose.matata11@gmail.com>
2022-12-15 04:42:35 -0800

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,5 +14,5 @@ int main(void)
 	printf("Size of a long int: %d byte(s)\n", size of(long int));
 	printf("Size of a long long int: %d byte(s)\n", size of(long long int));
 	printf("Size of a float: %d byte(s)\n", size of(float));
-	return(0)
+	return(0);
 }
