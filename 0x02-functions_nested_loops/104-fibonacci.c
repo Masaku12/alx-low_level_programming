fb3804e46a60b7c323cd7c2c5c3c7a682d229ce0
Ambrose Matata Sammy <ambrose.matata11@gmail.com>
2022-12-29 12:23:16 -0800

    This code finds and prints the first 98 Fibonacci numbers, starting with 1 and 2

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -21,13 +21,12 @@ int main(void)
 		b = sum;
 		printf(", %lu", sum);
 	}
-	
 	a_head = a / 1000000000;
 	a_tail = a % 1000000000;
 	b_head = b / 1000000000;
 	b_tail = b / 1000000000;
 
-	for (count = 93; count < 99; count++)
+	for (; count < 99; count++)
 	{
 		overflow = (a_tail + b_tail) / 1000000000;
 		sum_head = (a_head + b_head) + overflow;
