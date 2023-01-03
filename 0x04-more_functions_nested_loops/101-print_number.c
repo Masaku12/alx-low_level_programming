8e0c80ac675863e5f01ebc27c38ba8f11246f800
Ambrose Matata Sammy <ambrose.matata11@gmail.com>
2023-01-03 05:39:56 -0800

    This function prints an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,18 +8,18 @@
 
 void print_number(int n)
 {
-	unsigned int num = i;
-	num = i;
+	unsigned int num = n;
+	num = n;
 
-	if (i < 0)
+	if (n < 0)
 	{
 		_putchar('-');
 		num = -num;
 	}
 
-	if ((num / 10) > 0)
-		print_number(num / 10);
+	if ((n / 10) > 0)
+		print_number(n / 10);
 
-	_putchar((num % 10) + '0');
+	_putchar((n % 10) + '0');
 }
 
