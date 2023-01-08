f2ce29a54a9dc1dd8e62c650f98f923bbc5363e5
Ambrose Matata Sammy <ambrose.matata11@gmail.com>
2023-01-07 22:20:18 -0800

    This function encodes a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,17 +8,18 @@
 
 char *leet(char *str)
 {
-	int indx 1, indx 2;
-	int a[11] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
-	int b[11] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+	int indx 1;
+	int indx 2;
+	int i[11] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int j[11] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
 	for (indx1 = 0; str[indx1] != '\0'; indx1++)
 	{
-		for (indx2 = 0; a[indx2] != '\0'; indx2++)
+		for (indx2 = 0; i[indx2] != '\0'; indx2++)
 		{
-			if (str[indx1] == a[indx2])
+			if (str[indx1] == i[indx2])
 			{
-				str[indx1] = b[indx2];
+				str[indx1] = j[indx2];
 			}
 		}
 	}
