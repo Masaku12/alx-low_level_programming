e24ea8a07022b73652bca50c37ae83b1bfbc6ea9
Ambrose Matata Sammy <ambrose.matata11@gmail.com>
2023-01-09 03:54:27 -0800

    This function encodes a string using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,7 +8,7 @@
 
 char *rot13(char *str)
 {
-	int i[53] = {'a', 'b', 'c', 'd', 'e', 'f',
+	int i[52] = {'a', 'b', 'c', 'd', 'e', 'f',
 		'g', 'h', 'i', 'j', 'k', 'l', 'm',
 		'n', 'o', 'p', 'q', 'r', 's', 't',
 		'u', 'v', 'w', 'x', 'y', 'z', 'A',
@@ -16,12 +16,14 @@ char *rot13(char *str)
 		'I', 'J', 'K', 'L', 'M', 'N', 'O',
 		'P', 'Q', 'R', 'S', 'T', 'U', 'V',
 		'W', 'X', 'Y', 'Z'};
-	int j[53] = {'n', 'o', 'p', 'q', 'r', 's',
+	int j[52] = {'n', 'o', 'p', 'q', 'r', 's',
 		't', 'u', 'v', 'w', 'x', 'y', 'z',
 		'a', 'b', 'c', 'd', 'e', 'f', 'g',
 		'h', 'i', 'j', 'k', 'l', 'm', 'N',
 		'O', 'P', 'Q', 'R', 'S', 'T', 'U',
-		'V', 'W', 'X', 'Y', 'Z'};
+		'V', 'W', 'X', 'Y', 'Z', 'A', 'B',
+		'C', 'D', 'E', 'F', 'G', 'H', 'I',
+		'J', 'K', 'L', 'M'};
 	int index1, index2;
 
 	for (index1 = 0; str[index1] != '\0'; index1++)
