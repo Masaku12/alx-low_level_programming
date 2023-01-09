986f86a33920bfacd1adeeb6ed06cf92e1e0636f
Ambrose Matata Sammy <ambrose.matata11@gmail.com>
2023-01-09 04:54:42 -0800

    A preliminary commit on one of the advanced tasks

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* infinite_add - This function adds two numbers
+* @n1: This string contains the first number
+* @n2: This string contaisn the second number
+* @r: the buffer storing the result
+* @size_r: The buffer size
+* Return: pointer to the result if r can store the sum
+*         otherwise 0
+*/
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+
+
