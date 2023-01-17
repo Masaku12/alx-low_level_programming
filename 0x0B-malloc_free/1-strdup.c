90be97f4b4aa36b18cbfaba1be98b40e78a2c9a3
Ambrose Matata Sammy <ambrose.matata11@gmail.com>
2023-01-17 02:37:05 -0800

    This function returns a pointer

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup -  function returns a pointer to a new string
+ * which is a duplicate of the string @str
+ * @str: A string being duplicated
+ * Return: A pointer
+ */
+
+char *_strdup(char *str);
+{
+	unsigned int i;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		continue;
+	s = malloc((i * sizeof(char)) + 1);
+
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		s[i] = str[i];
+
+	return (s);
+}
