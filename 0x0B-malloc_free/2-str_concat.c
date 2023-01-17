56acc77eec6ad70239760ed2004e3b0ab6ef2280
Ambrose Matata Sammy <ambrose.matata11@gmail.com>
2023-01-17 05:18:49 -0800

    This function adds two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -29,10 +29,10 @@ char *str_concat(char *s1, char *s2)
 	if (concat_str == NULL)
 		return (NULL);
 
-	for (i = 0; s1[i]; = i++)
+	for (i = 0; s1[i]; i++)
 		concat_str[concat_index++] = s1[i];
 
-	for (i = 0; s2[i]; = 1++)
+	for (i = 0; s2[i]; i++)
 		concat_str[concat_index++] = s2[i];
 
 	return (concat_str);
