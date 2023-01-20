e0382fa03f835766fa27ba5d6fd3e0c3af865519
Ambrose Matata Sammy <ambrose.matata11@gmail.com>
2023-01-20 09:20:08 -0800

    This function creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,12 +3,12 @@
 #include "dog.h"
 
 /**
- * _slen - Finds the string length
+ * s_len - Finds the string length
  * @str: The string under review
  * Return: The string length
  */
 
-int _slen(char *str)
+int s_len(char *str)
 {
 	int len = 0;
 
@@ -18,13 +18,13 @@ int _slen(char *str)
 }
 
 /**
- * _scpy - copies a string
+ * s_cpy - copies a string
  * @src: The origin of string under review
  * @dest: The port of call that will store the copied string
  * Return: The pointer to @dest
  */
 
-char *_scpy(char *dest, char *src)
+char *s_cpy(char *dest, char *src)
 {
 	int index;
 
@@ -45,32 +45,41 @@ char *_scpy(char *dest, char *src)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *k9;
-
-	if (name == NULL || age < 0 || owner == NULL)
-		return (NULL);
+	char *k9_name;
+	char *k9_owner;
 
 	k9 = malloc(sizeof(dog_t));
+
 	if (k9 == NULL)
 		return (NULL);
-
-	k9->name = malloc(sizeof(char) * (_slen(name) + 1));
-	if (k9->name == NULL)
-	{
-		free(k9);
-		return (NULL);
-	}
-
-	k9->owner = malloc(sizeof(char) * (_slen(name) + 1));
-	if (k9->owner == NULL)
-	{
-		free(k9->name);
-		free(k9);
-		return (NULL);
-	}
-
-	k9->name = _scpy(k9->name, name);
 	k9->age = age;
-	k9->owner = _scpy(k9->owner, owner);
+
+	if (name != NULL)
+	{
+		k9_name = malloc((s_len(name) * sizeof(char)) + 1);
+		if (k9_name == NULL)
+		{
+			free(k9);
+			return (NULL);
+		}
+		k9->name = s_cpy(name, k9_name);
+	}
+	else
+		k9->name = NULL;
+	if (owner == NULL)
+	{
+		k9_owner = malloc((s_len(owner) * sizeof(char)) + 1);
+		if (k9_owner == NULL)
+		{
+			free(k9->name);
+			free(k9->owner);
+			free(k9);
+			return (NULL);
+		}
+		k9->owner = s_cpy(owner, k9_owner);
+	}
+	else
+		k9->owner = NULL;
 
 	return (k9);
 }
