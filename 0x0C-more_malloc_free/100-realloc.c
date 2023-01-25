1b0c9bd674fb5bd420649f6e028e5ea604047be5
Ambrose Matata Sammy <ambrose.matata11@gmail.com>
2023-01-25 05:45:28 -0800

    This function reallocates a memory block using malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,31 +1,44 @@
-#include <stdlib.h>
 #include "main.h"
 
-/**
- * _realloc -  reallocates a memory block using malloc and free
+/** _realloc - reallocates a memory block
+ *
  * @ptr: pointer to the memory previously allocated with a call to malloc
- * @new_size: new size, in bytes of the new memory block
- * @old_size: size, in bytes, of the allocated space for ptr
- * Return: Pointer to allocated memory
- *	   NULL if new_size == 0 & ptr is not NULL
+ * @old_size: size of ptr
+ * @new_size: size of the new memory to be allocated
+ *
+ * Return: pointer to the address of the new memory block
  */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	if (new_size == old_size)
-		return (ptr);
+	void *temp_block;
+	unsigned int i;
 
 	if (ptr == NULL)
-		ptr = malloc(new_size);
+	{
+		temp_block = malloc(new_size);
+		return (temp_block);
+	}
+	else if (new_size == old_size)
+		return (ptr);
 
-	if (new_size == 0 && ptr != NULL)
+	else if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-
-	free(ptr);
-	ptr = malloc(new_size);
-
-	return (ptr);
+	else
+	{
+		temp_block = malloc(new_size);
+		if (temp_block != NULL)
+		{
+			for (i = 0; i < min(old_size, new_size); i++)
+				
+				*((char *)temp_block + i) = *((char *) ptr + i);
+			free(ptr);
+			return (temp_block);
+		}
+		else
+			return (NULL);
+	}
 }
