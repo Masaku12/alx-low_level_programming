00c3c363931549489ae3aa37e83794abd3ec6b40
Ambrose Matata Sammy <ambrose.matata11@gmail.com>
2023-01-31 01:52:42 -0800

    Added comments to a function that frees a list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -5,16 +5,23 @@
  * @head: head of the list
  *
  */
-void free_list(list_t *head)
+void free_list(list_t *head) /* The function free_list takes a pointer */
+				/* to a list_t as its parameter */
 {
-	list_t *tmp;
+	list_t *tmp; /* This function creates a temp file to a */
+			/* list_t called tmp */
 
-	tmp = head;
+	tmp = head; /* This sets tmp to point to the same thing */
+		    /* head is point to */
+	/* The function enters a while loop that will run as long */
+	/* as head is not NULL*/
 	while (head)
 	{
 		head = head->next;
-		free(tmp->str);
-		free(tmp);
-		tmp = head;
+		free(tmp->str); /* Frees the str that tmp points to */
+		free(tmp); /* Frees the memory that tmp points to */
+		tmp = head; /* Sets tmp to point to the same thing */
+			    /* that heads points to and returns nothing */
+
 	}
 }
